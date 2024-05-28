bd38388af0b959aa5a50423bf916305c07be6fe8
axlotlx <tejayadav435@gmail.com>
2024-05-28 11:04:44 +0530

    Embeddable annotation used

--- HibernateDemo/src/main/java/com/teja/HibernateDemo/App.java
+++ HibernateDemo/src/main/java/com/teja/HibernateDemo/App.java
@@ -14,9 +14,20 @@ public class App
     public static void main( String[] args )
     {
     		Student s1 = new Student();
+    		
+    		StudentName sn = new StudentName();
+    		sn.setFname("Teja");
+    		sn.setLname("Sanniboyina");
+    		sn.setMname("Guna");
+    		
 //    		s1.setRollNo(10);
 //    		s1.setName("Teja");
 //    		s1.setMarks(90);
+    		
+    		s1.setRollNo(101);
+    		s1.setMarks(60);
+    		s1.setSname(sn);
+    	
     		
     		Configuration conf = new Configuration().configure().addAnnotatedClass(Student.class);// Configuration is a class
     		
@@ -30,9 +41,10 @@ public class App
     		//ACID properties
     		Transaction ts = session.beginTransaction();
     		
-    		s1 = (Student)session.get(Student.class, 101);// to get the data from the database ,
+    	//	s1 = (Student)session.get(Student.class, 101);// to get the data from the database ,
     		//get method takes 2 parameters ( class name, primary key) and returns the Object of Object so typecast  it . 
-    				session.save(s1);
+    				
+    		session.save(s1);
     				ts.commit();
     				
     				System.out.println(s1);
--- HibernateDemo/src/main/java/com/teja/HibernateDemo/Student.java
+++ HibernateDemo/src/main/java/com/teja/HibernateDemo/Student.java
@@ -18,7 +18,8 @@ public class Student {
 	private int rollNo;
 	
 	//@Transient()  //excludes name as column of the table
-	private String name;
+	//private String name;
+	private StudentName sname;
 	
 	//@Column(name="student_marks")// to set the explicit column name
 	private int marks;
@@ -29,23 +30,38 @@ public class Student {
 	public void setRollNo(int rollNo) {
 		this.rollNo = rollNo;
 	}
-	public String getName() {
-		return name;
+	
+	
+//	public String getName() {
+//		return name;
+//	}
+//	public void setName(String name) {
+//		this.name = name;
+//	}
+	public StudentName getSname() {
+		return sname;
 	}
-	public void setName(String name) {
-		this.name = name;
+	public void setSname(StudentName sname) {
+		this.sname = sname;
 	}
+	
 	public int getMarks() {
 		return marks;
 	}
+
 	public void setMarks(int marks) {
 		this.marks = marks;
 	}
 	@Override
 	public String toString() {
-		return "Student [rollNo=" + rollNo + ", name=" + name + ", marks=" + marks + "]";
+		return "Student [rollNo=" + rollNo + ", sname=" + sname + ", marks=" + marks + "]";
 	}
 	
+//	@Override
+//	public String toString() {
+//		return "Student [rollNo=" + rollNo + ", name=" + name + ", marks=" + marks + "]";
+//	}
+//	
 	
 	
 
