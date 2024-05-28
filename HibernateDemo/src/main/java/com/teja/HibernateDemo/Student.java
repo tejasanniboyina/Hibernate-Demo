9a5222554e1a701232978bbae11dd162c68ec23d
axlotlx <tejayadav435@gmail.com>
2024-05-28 10:21:11 +0530

    Annotations added

--- HibernateDemo/src/main/java/com/teja/HibernateDemo/Student.java
+++ HibernateDemo/src/main/java/com/teja/HibernateDemo/Student.java
@@ -1,20 +1,26 @@
 package com.teja.HibernateDemo;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.Transient;
 
 
 // annotation to indicate this class is the one that is going to be a table
 
 @Entity()
-@Table(name="student_tble")// table name can be given like this too
+@Table(name="student_table")// table name can be given like this too
 public class Student {
 	
 	// annotation to make PRIMARY KEY (PRIMARY KEY must me available)
 	@Id
 	private int rollNo;
+	
+	@Transient()  //excludes name as column of the table
 	private String name;
+	
+	@Column(name="student_marks")// to set the explicit column name
 	private int marks;
 	
 	public int getRollNo() {
